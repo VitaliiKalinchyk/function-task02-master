f2257e6dc647251067a27b85356fc56430a5485c
mmveres <mmveres@gmail.com>
2022-04-19 14:38:21 +0300

    function-task2-master

---
+++ src/main/java/com/epam/rd/autotasks/FunctionsTask2.java
@@ -0,0 +1,26 @@
+package com.epam.rd.autotasks;
+
+public class FunctionsTask2 {
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     * if set invalid arguments in method, then method must throws
+     * IllegalArgumentException
+     */
+    public static boolean isSorted(int[] array, SortOrder order) {
+        //TODO: Delete line below and write your own solution
+        throw new UnsupportedOperationException();
+    }
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     * if set invalid arguments in method, then method must throws
+     * IllegalArgumentException
+     */
+    public static int[] transform(int[] array, SortOrder order) {
+       //TODO: Delete line below and write your own solution
+        throw new UnsupportedOperationException();
+    }
+}
